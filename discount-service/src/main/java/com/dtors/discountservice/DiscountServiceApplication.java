5724e28fb49c6285415e6ea80f79a0350cfe227c
Jawad Siddiqui <jsiddiqui@wsgc.com>
2022-12-13 10:30:32 +0500

    Spring resilience4j circuit-breaker implementation

---
+++ discount-service/src/main/java/com/dtors/discountservice/DiscountServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dtors.discountservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscountServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscountServiceApplication.class, args);
+	}
+
+}
