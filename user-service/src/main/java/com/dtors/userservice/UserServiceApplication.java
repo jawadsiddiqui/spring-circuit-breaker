af15ed5416252d426840ac9b156f92e5f6000461
Jawad Siddiqui <jsiddiqui@wsgc.com>
2022-12-13 10:49:30 +0500

    no message

--- user-service/src/main/java/com/dtors/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/dtors/userservice/UserServiceApplication.java
@@ -1,11 +1,26 @@
 package com.dtors.userservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/user")
 public class UserServiceApplication {
 
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@GetMapping("/check")
+	public String getCatalogue(){
+
+		return "";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
